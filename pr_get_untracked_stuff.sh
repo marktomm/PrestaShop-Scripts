d12ab4fa23c43a1d8b8fad24d2906368ef6a2aaf
Mark Tomm <marktomm90@gmail.com>
2018-05-12 13:08:42 +0300

    chmod +x and pr_get_untracked_stuff use ssh key provided

--- pr_get_untracked_stuff.sh
+++ pr_get_untracked_stuff.sh
@@ -5,7 +5,7 @@
 checkFileExecOrExit pr_init_config
 . pr_init_config
 
-scp -r ${SERVER_USER}@${SERVER_HOST}:${SERVER_PATH}/img ${PRESTA_ROOT}.                                                                                                                                                                                                                                                     
-scp -r ${SERVER_USER}@${SERVER_HOST}:${SERVER_PATH}/download ${PRESTA_ROOT}.                                                                                                                                                                                                                                                
-scp -r ${SERVER_USER}@${SERVER_HOST}:${SERVER_PATH}/upload ${PRESTA_ROOT}.                                                                                                                                                                                                                                                  
-scp -r ${SERVER_USER}@${SERVER_HOST}:${SERVER_PATH}/log ${PRESTA_ROOT}.
+scp ${SSH_KEY_LOCATION+ -i ${SSH_KEY_LOCATION}} -r ${SERVER_USER}@${SERVER_HOST}:${SERVER_PATH}/img ${PRESTA_ROOT}.                                                                                                                                                                                                                                                     
+scp ${SSH_KEY_LOCATION+ -i ${SSH_KEY_LOCATION}} -r ${SERVER_USER}@${SERVER_HOST}:${SERVER_PATH}/download ${PRESTA_ROOT}.                                                                                                                                                                                                                                                
+scp ${SSH_KEY_LOCATION+ -i ${SSH_KEY_LOCATION}} -r ${SERVER_USER}@${SERVER_HOST}:${SERVER_PATH}/upload ${PRESTA_ROOT}.                                                                                                                                                                                                                                                  
+scp ${SSH_KEY_LOCATION+ -i ${SSH_KEY_LOCATION}} -r ${SERVER_USER}@${SERVER_HOST}:${SERVER_PATH}/log ${PRESTA_ROOT}.
